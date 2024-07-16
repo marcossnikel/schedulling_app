b9fc154db68dee79a493483530f7da9458830cb8
marcossnikel <marcosnikeldev@gmail.com>
2024-07-16 17:16:13 -0300

    remove variable

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { env } from "../../env";
 
-const supabaseURL = env.SUPABASE_URL;
-const supabaseKey = env.SUPABASE_SERVICE_KEY;
-
-export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_KEY);
+export const supabase = createClient(
+  env.SUPABASE_URL,
+  env.SUPABASE_SERVICE_KEY
+);
