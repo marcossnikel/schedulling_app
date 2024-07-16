0db333980ef5215161d1739289bb9d05abb3267b
marcossnikel <marcosnikeldev@gmail.com>
2024-07-15 21:13:12 -0300

    change driver and add dbCredentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { env } from "./env";
 export default defineConfig({
   dialect: "sqlite",
-  schema: "./src/schema.ts",
-  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  driver: "turso",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
+  },
 });
